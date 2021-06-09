91ddd980b6d1f304ce1dc9bdbca5c678dcc12636
luis <lp_santos29@outlook.com>
2021-06-09 16:55:00 -0300

    Update

--- get_next_line.h
+++ get_next_line.h
@@ -16,11 +16,11 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-int		get_next_line(int fd, char **line);
-size_t	ft_strlen(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *s);
+size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
-char	*ft_strdup(const char *s);
+int		get_next_line(int fd, char **line);
 
 #endif
